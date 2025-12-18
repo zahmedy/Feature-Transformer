2770b2d69fd61d4d830ccdbe43152c8a236fd340
Zayed Ahmed <zayed967@yahoo.com>
2025-12-18 10:27:43 -0500

    dtype drift numeric to object coercible

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -1,8 +1,23 @@
-import pandas as pd
+import numpy as np
 import pytest
 from src.transformer import FeatureTransformer
 
 
+def test_dtype_drift_numeric_to_object_coercible(make_df):
+    X, y = make_df()
 
-# TO be completed 
+    ft = FeatureTransformer()
+    ft.fit(X, y)
 
+    num_cols = X.select_dtypes(include="number").columns.tolist()
+
+    Z1 = ft.transform(X)
+
+    X2 = X.copy()
+    X2[num_cols[0]] = X2[num_cols[0]].astype(str)
+
+    Z2 = ft.transform(X2)
+
+    assert Z1.shape == Z2.shape
+    assert not np.isnan(Z2).any()
+    
